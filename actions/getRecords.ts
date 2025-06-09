1e4f2e081442c529a00a1ac31daffe29ded380bd
thaiwireproducts <thaiwireproducts1@gmail.com>
2025-06-09 13:03:59 +0700

    reportchart

---
+++ actions/getRecords.ts
@@ -0,0 +1,34 @@
+'use server';
+
+import { auth } from '@clerk/nextjs/server';
+import { Record } from '@/types/Record';
+import { db } from '@/app/lib/db';
+
+
+async function getRecords(): Promise<{
+  records?: Record[];
+  error?: string;
+}> {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return { error: 'User not found' };
+  }
+
+  try {
+    const records = await db.record.findMany({
+      where: { userId },
+      orderBy: {
+        date: 'desc', // Sort by the `date` field in descending order
+      },
+      take: 10, // Limit the request to 10 records
+    });
+
+    return { records };
+  } catch (error) {
+    console.error('Error fetching records:', error); // Log the error
+    return { error: 'Database error' };
+  }
+}
+
+export default getRecords;
